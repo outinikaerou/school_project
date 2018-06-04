ad1a129fae81433ebe70ac427932e85d849f54c1
unknown <st3001@PC01.itabashi-tc.ac.jp>
2018-06-04 15:30:20 +0900

    bye

--- Vivado/CNT60_ALL/CNT_ALL.v
+++ Vivado/CNT60_ALL/CNT_ALL.v
@@ -15,11 +15,11 @@ parameter SEC1_MAX = 12000000; // 12MHz
 always @(posedge CLK or posedge RESET)
   begin
      if (RESET == 1'b1)
-       tmp_count <= 23'h000000;
+       tmp_count <= 24'h000000;
      else if (ENABLE == 1'b1)
-       tmp_count <= 23'h000000;
+       tmp_count <= 24'h000000;
      else
-       tmp_count <= tmp_count + 23'h1;
+       tmp_count <= tmp_count + 24'h1;
   end
 
 assign ENABLE = (tmp_count == (SEC1_MAX - 1))? 1'b1 : 1'b0;
---
+++ Vivado/CNT60_ALL/ENABLE_GEN.v
@@ -0,0 +1,21 @@
+module ENABLE_GEN(CLK, ENABLE_GEN)
+input CLK
+output ENABLE_GEN
+
+reg [23:0] tmp_count;
+wire ENABLE;
+
+parameter SEC1_MAX = 12000000; // 12MHz
+
+assign ENABLE = (tmp_count == (SEC1_MAX - 1))? 1'b1 : 1'b0;
+
+always @(posedge CLK or posedge RESET)
+  begin
+     if (RESET == 1'b1)
+       tmp_count <= 24'h000000;
+     else if (ENABLE == 1'b1)
+       tmp_count <= 24'h000000;
+     else
+       tmp_count <= tmp_count + 24'h1;
+  end
+  
